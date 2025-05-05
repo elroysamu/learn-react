a4ca7ca3b4096811e6cd808f4f5cde2ba1c164a7
elroysaamu <elroy.devadason@finos.tech>
2025-05-05 17:48:18 +0530

    with transition

--- src/components/script.js
+++ src/components/script.js
@@ -1,4 +1,4 @@
-import React, {lazy, useState} from 'react';
+import React, {startTransition, useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import { Header } from './Header';
 import { Body } from './Body';
@@ -8,30 +8,59 @@ import { Contact } from './Contact';
 import { Error } from './Error';
 import { RestaurantMenuCard } from './RestaurantMenuCard';
 
-function App() {
-    const [query, setQuery] = useState('');
-    const [results, setResults] = useState([]);
+// function App() {
+//     const [query, setQuery] = useState('');
+//     const [results, setResults] = useState([]);
     
-    const bigList = Array.from({ length: 10000 }, (_, i) => `Item ${i}`);
+//     const bigList = Array.from({ length: 10000 }, (_, i) => `Item ${i}`);
    
-    function handleChange(e) {
-    const value = e.target.value;
-    setQuery(value);
+//     function handleChange(e) {
+//     const value = e.target.value;
+//     setQuery(value);
    
-    // Filtering is immediate (UI will lag with big lists)
-    const filtered = bigList.filter(item => item.includes(value));
-    setResults(filtered);
-    }
+//     // Filtering is immediate (UI will lag with big lists)
+//     const filtered = bigList.filter(item => item.includes(value));
+//     setResults(filtered);
+//     }
    
-    return (
-    <div>
-    <input value={query} onChange={handleChange} />
-    <ul>
-    {results.map((item, i) => <li key={i}>{item}</li>)}
-    </ul>
-    </div>
-    );
-   }
+//     return (
+//     <div>
+//     <input value={query} onChange={handleChange} />
+//     <ul>
+//     {results.map((item, i) => <li key={i}>{item}</li>)}
+//     </ul>
+//     </div>
+//     );
+//    }
+
+
+
+// function App() {
+//     const [query, setQuery] = useState('');
+//     const [results, setResults] = useState([]);
+   
+//     const bigList = Array.from({ length: 10000 }, (_, i) => `Item ${i}`);
+   
+//     function handleChange(e) {
+//     const value = e.target.value;
+//     setQuery(value); // Urgent update – happens immediately
+   
+//     // This update can be delayed – mark as transition
+//     startTransition(() => {
+//     const filtered = bigList.filter(item => item.includes(value));
+//     setResults(filtered);
+//     });
+//     }
+   
+//     return (
+//     <div>
+//     <input value={query} onChange={handleChange} />
+//     <ul>
+//     {results.map((item, i) => <li key={i}>{item}</li>)}
+//     </ul>
+//     </div>
+//     );
+//    }
 
 
 
